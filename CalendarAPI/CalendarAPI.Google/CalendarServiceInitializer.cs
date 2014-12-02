7ab9ee6252dff528fd9a1ef42945fa68746f993f
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-12-02 18:34:27 +0200

    refacturing

--- CalendarAPI/CalendarAPI.Google/CalendarServiceInitializer.cs
+++ CalendarAPI/CalendarAPI.Google/CalendarServiceInitializer.cs
@@ -12,18 +12,12 @@ namespace CalendarAPI.Google
         private const string ServiceAccountEmail = "847753983802-6qbjred3ht7beabof68s9dapuq94912i@developer.gserviceaccount.com";
         private const string GoogleApplicationName = "CalendarApiTest";
         private const string CertificateRelativePath = "../../CalendarAPI.Google/Certificate/CalendarApiTest-a03ab29c21cd.p12";
-        private X509Certificate2 certificate;
+        private X509Certificate2 serviceCertificate;
 
         public CalendarServiceInitializer()
         {
-            var appDomain = AppDomain.CurrentDomain;
-            var basePath = appDomain.RelativeSearchPath ?? appDomain.BaseDirectory;
-            var combinedPath = Path.Combine(basePath, CertificateRelativePath);
-
-            // normalizes the absolute and the relative path combination
-            var fullPath = Path.GetFullPath(combinedPath);
-
-            this.certificate = new X509Certificate2(fullPath, "notasecret", X509KeyStorageFlags.Exportable);
+            var fullPath = this.GetFullPathToCertificate();
+            this.serviceCertificate = new X509Certificate2(fullPath, "notasecret", X509KeyStorageFlags.Exportable);
         }
 
         public CalendarService GetCalendarService()
@@ -45,9 +39,21 @@ namespace CalendarAPI.Google
                 new ServiceAccountCredential.Initializer(ServiceAccountEmail)
                 {
                     Scopes = new[] { CalendarService.Scope.Calendar }
-                }.FromCertificate(this.certificate));
+                }.FromCertificate(this.serviceCertificate));
 
             return credentials;
+        }
+
+        private string GetFullPathToCertificate()
+        {
+            var appDomain = AppDomain.CurrentDomain;
+            var basePath = appDomain.RelativeSearchPath ?? appDomain.BaseDirectory;
+            var combinedPath = Path.Combine(basePath, CertificateRelativePath);
+
+            // normalizes the absolute and the relative path combination
+            var fullPath = Path.GetFullPath(combinedPath);
+
+            return fullPath;
         }
     }
 }
